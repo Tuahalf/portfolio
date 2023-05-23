9f7ecebb8ddbc497b7b1678df5bd59f0aa1b5e73
Aurélien Flahaut <flahaut.aur@gmail.com>
2023-05-23 16:41:32 +0200

    fini

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 const folders = document.querySelectorAll('.folder');
 
 folders.forEach(folder => {
-    folder.addEventListener('click', () => {
-        const folderContent = folder.nextElementSibling;
-        folderContent.classList.toggle('folder-content-show');
-    });
+  const folderContent = folder.nextElementSibling;
+  const folderName = folder.textContent.trim();
+  
+  const isOpen = localStorage.getItem(folderName) === 'true';
+  
+  if (isOpen) {
+    folderContent.classList.add('folder-content-show');
+  }
+  
+  folder.addEventListener('click', () => {
+    folderContent.classList.toggle('folder-content-show');
+
+    const isOpen = folderContent.classList.contains('folder-content-show');
+    localStorage.setItem(folderName, isOpen ? 'true' : 'false');
+  });
 });
+
+
+
+
